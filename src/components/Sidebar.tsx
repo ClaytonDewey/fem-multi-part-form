9aac5dfbfc0e709603eb2b72bc405ff0d8384e2b
ClaytonDewey <clay@dryadmedia.com>
2026-01-20 11:22:47 -0600

    refactor: Wrap sidebar in container div

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,11 +2,13 @@ import { Step } from '.';
 
 const Sidebar = () => {
   return (
-    <section className='sidebar'>
-      <Step step={1} active={true} />
-      <Step step={2} active={false} />
-      <Step step={3} active={false} />
-      <Step step={4} active={false} />
+    <section className='sidebar-container'>
+      <div className='sidebar'>
+        <Step step={1} active={true} />
+        <Step step={2} active={false} />
+        <Step step={3} active={false} />
+        <Step step={4} active={false} />
+      </div>
     </section>
   );
 };
